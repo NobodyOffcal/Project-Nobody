33c51f5955961b430039fb9284039fd34fd7af70
NobodyOffcal <100217070+NobodyOffcal@users.noreply.github.com>
2022-05-13 17:48:31 +0300

    Add files via upload

---
+++ Api.lua
@@ -0,0 +1,15 @@
+--Simple Api V1.5
+
+return
+{
+    
+    "Project_Nobody_Servers_True",
+    "Status_True",
+    "Maintrance_Break_False",
+    "Leaked_False",
+    "Current_Version_V2.6.0",
+    "Current_Key_dc168832871d3288ceeae7f8cd6ac92c",
+    "Data_Check_True",
+    "Current_Secret_Key_Nil",
+    
+}
