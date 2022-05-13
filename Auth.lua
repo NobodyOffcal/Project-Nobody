b8c5fc3ed3f6d2d2d70428763a866987f16e8c04
NobodyOffcal <100217070+NobodyOffcal@users.noreply.github.com>
2022-05-13 17:49:54 +0300

    Add files via upload

---
+++ Auth.lua
@@ -0,0 +1,120 @@
+--Simple Auth V1.5
+
+return
+{
+    
+    --SusNobody#4877
+
+    "NobodyOffcal_Banned_False",
+    "NobodyOffcal_Rank_Administrator",
+    "NobodyOffcal_Key_True",
+    "NobodyOffcal_Key_Time_Lifetime",
+    "NobodyOffcal_Key_Status_Premium",
+    "NobodyOffcal_Hwid_613EABB2-0ABE-4FA7-8EFC-9E876637FD21",
+    
+    --Video
+    
+    "bobloxvsnobody_Banned_False",
+    "bobloxvsnobody_Rank_Administrator",
+    "bobloxvsnobody_Key_True",
+    "bobloxvsnobody_Key_Time_Lifetime",
+    "bobloxvsnobody_Key_Status_Premium",
+    "bobloxvsnobody_Hwid_613EABB2-0ABE-4FA7-8EFC-9E876637FD21",
+    
+    --me756j#7490
+    --Re Buyed Account
+
+    "Noname325632_Banned_False",
+    "Noname325632_Rank_Buyer",
+    "Noname325632_Key_True",
+    "Noname325632_Key_Time_Lifetime",
+    
+    --yourdailydoseofinternet#9491
+    --Re Buyed Account
+    
+    "shadow_hunter039_Banned_False",
+    "shadow_hunter039_Rank_Administrator",
+    "shadow_hunter039_Key_True",
+    "shadow_hunter039_Key_Time_Lifetime",
+    
+    --susbody#9606
+    
+    "itsmihaaaai_Banned_False",
+    "itsmihaaaai_Rank_Buyer",
+    "itsmihaaaai_Key_True",
+    "itsmihaaaai_Key_Time_Lifetime",
+    
+    --Rezn#2992
+    
+    "jyhtgrfedr777_Banned_False",
+    "jyhtgrfedr777_Rank_Buyer",
+    "jyhtgrfedr777_Key_True",
+    "jyhtgrfedr777_Key_Time_Lifetime",
+    "jyhtgrfedr777_Key_Status_Regular",
+        
+    --Alec <9#1445
+    
+    "roguebrian12345_Banned_False",
+    "roguebrian12345_Rank_Buyer",
+    "roguebrian12345_Key_True",
+    "roguebrian12345_Key_Time_Lifetime",
+    
+    --Fatee#0555
+    
+    "jhariez4_Banned_False",
+    "jhariez4_Rank_Administrator",
+    "jhariez4_Key_True",
+    "jhariez4_Key_Time_Lifetime",
+
+    --!Candys#5776
+    
+    "Pykachuzs_Banned_False",
+    "Pykachuzs_Rank_Buyer",
+    "Pykachuzs_Key_True",
+    "Pykachuzs_Key_Time_Lifetime",
+    
+    --Giannis#4299
+
+    "S7FZ_Banned_False",
+    "S7FZ_Rank_Buyer",
+    "S7FZ_Key_True",
+    "S7FZ_Key_Time_Lifetime",
+      
+    --Catchii#1337
+    
+    "justingamer1154_BannedTrue",
+    "justingamer1154_Rank_Buyer",
+    "justingamer1154_Key_False",
+    "justingamer1154_Key_Time_Lifetime",
+    
+    --dont look my bio#8983
+    
+    "aabcde123456123456_Banned_False",
+    "aabcde123456123456_Rank_Trial",
+    "aabcde123456123456_Key_False",
+    "aabcde123456123456_Key_Time_10Days",
+    
+    --RobuxFlash#5529
+    
+    "PsxBank23_Banned_False",
+    "PsxBank23_Rank_Buyer",
+    "PsxBank23_Key_True",
+    "PsxBank23_Key_Time_Lifetime",
+    
+    --B4D4$$ B#9999
+    
+    "ksjjejj_Banned_False",
+    "ksjjejj_Rank_Trial",
+    "ksjjejj_Key_True",
+    "ksjjejj_Key_Time_Lifetime",
+    "ksjjejj_Key_Status_Regular",
+    
+    --mannftx#0001
+    
+    "mannftx_Banned_False",
+    "mannftx_Rank_Buyer",
+    "mannftx_Key_True",
+    "mannftx_Key_Time_Lifetime",
+    "mannftx_Key_Status_Premium",
+
+}
