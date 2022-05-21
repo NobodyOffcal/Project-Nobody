25b60ebb9bc29a58a9f841cfcfe80cb639c8f4be
NobodyOffcal <100217070+NobodyOffcal@users.noreply.github.com>
2022-05-21 13:20:20 +0300

    Update Api.lua

--- Api.lua
+++ Api.lua
@@ -7,7 +7,7 @@ return
     "Status_True",
     "Maintrance_Break_False",
     "Leaked_False",
-    "Current_Version_V2.7.0",
+    "Current_Version_V2.8.0",
     "Current_Key_dc168832871d3288ceeae7f8cd6ac92c",
     "Data_Check_True",
     "Current_Secret_Key_Nil",
